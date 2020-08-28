cde237d96219f1931b934d2f6fef1d5166993a99
fauziapriana29 <fauzicoding@gmail.com>
2020-08-28 15:15:50 +0700

    Todo list baru

--- list.js
+++ list.js
@@ -1,3 +1,28 @@
+document.getElementById("addList").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const date = document.getElementById("inputDate").value;
+    const time = document.getElementById("inputTime").value;
+    const list = document.getElementById("inputList").value;
+
+    const data = {
+        date,
+        time,
+        list,
+    }
+    // console.log(data)
+    axios.post("http://localhost:3000/list", data).then((respone) => {
+        console.log("berhasil ditambah" + respone)
+        window.location.reload(true)
+    })
+    .catch((error) => {
+        console.log("data gagal ditambah" + error)
+    })
+})
+
+
+
+
 let data = []
 
 axios.get("http://localhost:3000/list").then((respone) => {
